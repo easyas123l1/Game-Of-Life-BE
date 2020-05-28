073368b13ba055072838bfdace149da08cabb461
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-05-28 12:44:59 -0500

    hardcode variables

--- models/config.ts
+++ models/config.ts
@@ -2,11 +2,11 @@ import "https://deno.land/x/dotenv/load.ts";
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 const client = new Client({
-  user: Deno.env.get("USER"),
-  database: Deno.env.get("DATABASE"),
-  hostname: Deno.env.get("DATABASE_URL"),
+  user: "rxgiclvddkrgdc",
+  database: "dbi6r94vgt52nj",
+  hostname: "ec2-34-200-72-77.compute-1.amazonaws.com",
   port: 5432,
-  password: Deno.env.get("PASSWORD"),
+  password: "d067648bd43f0b4c136d32885a217024ed98a983caee9efcba1067cfa5148eb6",
 });
 
 await client.connect();
